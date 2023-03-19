45d6f4234e207b3eb50a4b7ee3b8cdcd51ee0607
brandon <brandon@brandons-MacBook-Pro.local>
2023-03-19 15:28:15 -0400

    rename consts for FILE and BACKUP_FILE; put write invocations into a function

--- ui/repository/categoryRepository.js
+++ ui/repository/categoryRepository.js
@@ -3,17 +3,17 @@ const path = require('path');
 const rootDir = require('../util/rootDir');
 const fileio = require('../util/fileio');
 
-const filename = path.join(rootDir, 'data', 'categoryList.json');
-const BACKUP_FILE = `${filename}.backup`;
+const FILE = path.join(rootDir, 'data', 'categoryList.json');
+const BACKUP_FILE = `${FILE}.backup`;
 
 module.exports = (function createCategoryRepository() {
     return {
         fetchAll: async () => {
-            const list = await fileio.parseJson(filename);
+            const list = await fileio.parseJson(FILE);
             return list;
         },
         findById: async function(id) {
-            const list = await fileio.parseJson(filename);
+            const list = await fileio.parseJson(FILE);
             list.filter(category => category.id === id);
             if (list.size === 0) {
                 return null;
@@ -21,9 +21,9 @@ module.exports = (function createCategoryRepository() {
             return list[id];
         },
         append: async function(category) {
-            const list = await fileio.parseJson(filename);
+            const list = await fileio.parseJson(FILE);
             list.push(category);
-            fileio.writeJson(filename, JSON.stringify(list));
+            fileio.writeJson(FILE, JSON.stringify(list));
             fileio.writeJson(BACKUP_FILE, JSON.stringify(list));
         },
     };
--- ui/repository/noteRepository.js
+++ ui/repository/noteRepository.js
@@ -3,26 +3,26 @@ const path = require('path');
 const rootDir = require('../util/rootDir');
 const fileio = require('../util/fileio');
 
-const filename = path.join(rootDir, 'data', 'noteList.json');
-const BACKUP_FILE = path.join(rootDir, 'data', 'noteList.json.backup');
+const FILE = path.join(rootDir, 'data', 'noteList.json');
+const BACKUP_FILE = `${FILE}.backup`;
 
 module.exports = (function createNoteRepository() {
     function persistList(list) {
-        fileio.writeJson(filename, JSON.stringify(list));
+        fileio.writeJson(FILE, JSON.stringify(list));
         fileio.writeJson(BACKUP_FILE, JSON.stringify(list));
     }
     return {
         fetchAll: async () => {
-            const list = await fileio.parseJson(filename);
+            const list = await fileio.parseJson(FILE);
             return list;
         },
         append: async function(noteToSave) {
-            const list = await fileio.parseJson(filename);
+            const list = await fileio.parseJson(FILE);
             list.push(noteToSave);
             persistList(list);
         },
         findById: async function(id) {
-            const list = await fileio.parseJson(filename);
+            const list = await fileio.parseJson(FILE);
             list.filter(note => note.noteId === id);
             if (list.size === 0) {
                 return null;
@@ -30,7 +30,7 @@ module.exports = (function createNoteRepository() {
             return list[id];
         },
         update: async function(noteToSave) {
-            const list = await fileio.parseJson(filename);
+            const list = await fileio.parseJson(FILE);
             list
                 .filter(n => {
                     return n.noteId === noteToSave.noteId
