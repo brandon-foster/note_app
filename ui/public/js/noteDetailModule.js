1c01e4e2cc9ecb169647d6cb713592779ce5bdce
brandon <brandon@brandons-MacBook-Pro.local>
2023-04-08 23:38:23 -0400

    Detail nav improvements:
    Does not activate nav item upon entering h2.
    Resizing from xs to sm does not cause unexpected top.
    Does not become fixed if nav is too long.
    
    Squashed commit of the following:
    
    commit 7f446f3357fb367068fbabeb28e2daee474b5826
    Author: brandon <brandon@brandons-MacBook-Pro.local>
    Date:   Sat Apr 8 23:37:20 2023 -0400
    
        do not set position to fixed if detail nav is too long
    
    commit d037745413087187146de3ef931479e93e32bdb6
    Author: brandon <brandon@brandons-MacBook-Pro.local>
    Date:   Sat Apr 8 23:12:33 2023 -0400
    
        no empty nav items are rendered
    
    commit 5af40accdd3272da55c1a82a21a0de03bf3b9ee3
    Author: brandon <brandon@brandons-MacBook-Pro.local>
    Date:   Sat Apr 8 23:02:05 2023 -0400
    
        use row for originalOffsetTop
    
    commit 7a4079713dd90a171874bc8964ffba0898b75348
    Author: brandon <brandon@brandons-MacBook-Pro.local>
    Date:   Sat Apr 8 22:27:48 2023 -0400
    
        do not set nav elem as active upon entering h2
    
    commit 7fad8039e05c0516b11362f4a073e74e1d2888c3
    Author: brandon <brandon@brandons-MacBook-Pro.local>
    Date:   Sat Apr 8 22:27:06 2023 -0400
    
        refactor detail page

--- ui/public/js/noteDetailModule.js
+++ ui/public/js/noteDetailModule.js
@@ -1,12 +1,25 @@
 import createSideNavManager from './createSideNavManager.js';
 
+const FIXED_TOP = 50;
+
+function getAncestorOf(elem, selector) {
+    let parent = elem.parentElement;
+    while (parent !== null && !parent.matches(selector)) {
+        parent = parent.parentElement;
+        if (parent.matches(selector)) {
+            return parent;
+        }
+    }
+}
+
 function setupThreshhold(elem, onEnterThreshhold, onExitThreshhold) {
+    const row = getAncestorOf(elem, '.row');
     let toggleSwitch = false;
-    const originalOffsetTop = elem.offsetTop;
+    const originalOffsetTop = row.offsetTop;
     window.addEventListener('scroll', decideToSetFixed);
     decideToSetFixed();
     function decideToSetFixed() {
-        if (window.scrollY >= originalOffsetTop - 50) {
+        if (window.scrollY >= originalOffsetTop - FIXED_TOP) {
             if (toggleSwitch === false) {
                 onEnterThreshhold(elem);
                 toggleSwitch = true;
@@ -20,37 +33,49 @@ function setupThreshhold(elem, onEnterThreshhold, onExitThreshhold) {
         }
     }
 }
-function setWidthOfTo(srcElem, destElem) {
-    destElem.style.width = srcElem.clientWidth + 'px';
-}
-window.addEventListener('load', function() {
-    const detailNav = document.querySelector('#sideNav');
-    const wrapper = document.querySelector('.sideNavWrapper');
-    setWidthOfTo(wrapper, detailNav);
+const breakPointState = (function breakPointState() {
+    let isOutsideBound = undefined;
+    function onResize() {
+        const width = window.innerWidth;
+        isOutsideBound = width >= 576;
+        if (isOutsideBound) {
+            performDisplayOfSideNav();
+        }
+    }
+    return {
+        onResize: onResize,
+        isOutsideBound: function isOutsideBound() {
+            return isOutsideBound;
+        },
+    };
+}());
+window.addEventListener('resize', breakPointState.onResize);
+window.addEventListener('load', performDisplayOfSideNav);
+
+function performDisplayOfSideNav() {
+    const sideNav = document.querySelector('#sideNav');
+    const sideNavWrapper = document.querySelector('.sideNavWrapper');
     const sideNavManager = createSideNavManager({
-        detailNav: detailNav,
+        detailNav: sideNav,
     });
-    setupThreshhold(detailNav, function setFixed(elem) {
-        elem.style.position = 'fixed';
-        elem.style.top = '50px';
-    }, function unsetFixed(elem) {
-        elem.style.position = null;
-        elem.style.top = null;
-    });
+    if (breakPointState.isOutsideBound) {
+        sideNav.style.width = sideNavWrapper.clientWidth + 'px';
+        setupThreshhold(sideNav, function setFixed(elem) {
+            if (sideNavWrapper.offsetHeight < (window.innerHeight - FIXED_TOP)) {
+                elem.style.position = 'fixed';
+                elem.style.top = FIXED_TOP + 'px';
+            }
+        }, function unsetFixed(elem) {
+            elem.style.position = null;
+            elem.style.top = null;
+        });
+    }
     
     const h2List = document.getElementsByTagName('h2');
-    [...h2List].forEach(h2 => {
-        setupThreshhold(h2, function logEnter(elem) {
-            sideNavManager.setActiveViaScroll(elem.id);
-        },
-        function logExit(elem) {
-            sideNavManager.setActiveViaScroll(elem.id);
-        });
-    });
-    detailNav.addEventListener('click', function onDetailNavClick(e) {
+    sideNav.addEventListener('click', function onDetailNavClick(e) {
         if (e.target.matches('a')) {
             const headingId = e.target.dataset.heading;
             sideNavManager.setActiveViaClick(headingId);
         }
     });
-});
+}
